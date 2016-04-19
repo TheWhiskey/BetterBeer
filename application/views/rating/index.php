827c88534f145ec00d594c14cf1ae814f8263acf
Gretter74 <bo.sosti@gmail.com>
2016-04-20 00:32:16 +0200

    Dropdowns populated.
    Form dropdowns populated.
    header.php anchors added.
    Joins in rating model to get username and beername.

--- application/views/rating/index.php
+++ application/views/rating/index.php
@@ -2,9 +2,9 @@
 
 <?php foreach ($ratings as $rating_item): ?>
 
-        <h3>Beer: <?php echo $rating_item['beer_id']; ?></h3>
+        <h3>Beer: <?php echo $rating_item['beername']; ?></h3>
         <div class="main">
-                User: <?php echo $rating_item['user_id']; ?><br>
+                User: <?php echo $rating_item['username']; ?><br>
 				Rating: <?php echo $rating_item['rating']; ?><br>
 				
         </div>
