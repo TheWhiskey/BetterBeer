7d514a2fae77edf712791f7a91f2438f79b97448
Gretter74 <bo.sosti@gmail.com>
2016-04-20 21:39:33 +0200

    First addition of css
    Added css.
    UTF-8 encoding added to beer index.php. That should be done for all
    files with danish language.

--- application/views/rating/create.php
+++ application/views/rating/create.php
@@ -2,16 +2,30 @@
 
 <?php echo validation_errors(); ?>
 
+<?php echo $user_name; ?>
+
 <?php echo form_open('rating/create'); ?>
 
-    <label for="user_id">User</label>
-    <input type="input" name="user_id" /><br />
-
-    <label for="beer_id">Beer</label>
-    <input type="input" name="beer_id" /><br />
+    
+	
+	<label for="beer_id">Beer</label>
+	<select name="beer_id">
+	
+		<?php var_dump($b);?>
+		
+		<?php foreach($beer as $b): ?>
+		<?php echo '<option value="'.$b['id'].'">' . $b['name']; ?> </option>
+		<?php endforeach; ?>
+	</select>
 
 	<label for="rating">Rating</label>
-    <input type="input" name="rating" /><br />
+    <select name="rating">
+		<option value="1">1</option>
+		<option>2</option>
+		<option>3</option>
+		<option>4</option>
+		<option>5</option>
+	</select>
 
     <input type="submit" name="submit" value="Create news item" />
 
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,12 +1,16 @@
 <html>
 	<head>
-		<title>CodeIgniter Tutorial</title>
+		<title>Better Beer</title>
+		<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
 	</head>
 	<body>
 	
-		<h1><?php echo $title; ?></h1>
+		
+		<div class="header_menu">
 		<?php echo anchor('beer','Beer Archive'); ?><br>
 		<?php echo anchor('beer/create','Beer create'); ?><br>
 		<?php echo anchor('rating','Ratings archive'); ?><br>
 		<?php echo anchor('rating/create','Rating create'); ?><br>
+		</div>
+		<h2><?php echo $title; ?></h2>
 		
